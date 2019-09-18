4c47fad8d6d53e7ca4c3d4ffac6a748b08e39662
Chris King <chris.aa.king@gmail.com>
2019-09-18 19:00:48 +0200

    02 and 03 finished

--- Ruby-Projects/ruby-exercises/03_simon_says/simon_says.rb
+++ Ruby-Projects/ruby-exercises/03_simon_says/simon_says.rb
@@ -1,1 +1,47 @@
 #write your code here
+# def simon_says
+
+    def echo x
+        x.to_s
+    end
+    def shout x
+        x.to_s.upcase
+    end
+    def repeat(x, *y)
+        if y.length == 0 then
+            y = Array.new
+            y.push('2')
+        end
+            y = y.join.to_i
+            z = Array.new
+            y.times do
+                z.push(x)
+            end
+            z = z.join(" ").to_s
+            
+    end
+    def start_of_word x, y
+        y = y-1
+        x[0..y]
+    end
+    def first_word x
+        x.chomp
+        x.to_s
+        x = x.gsub(/\s+/m, ' ').strip.split(" ")
+        x[0]
+    end
+    def titleize x
+        x = x.gsub(/\s+/m, ' ').strip.split(" ")
+        z = Array.new
+        x.each do |word|
+            puts word
+            if word != "and" && word != "over" && word != "the" then
+            word = word.capitalize
+            end
+            z.push(word)
+        end
+        z[0] = z[0].capitalize
+        z.join(" ").to_s
+    end
+# end #end of Simon says
+titleize("war and peace")
