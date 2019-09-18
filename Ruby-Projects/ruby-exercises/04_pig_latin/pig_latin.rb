95d415e1b1a4a62d617d025b2759acaf49aeeb10
Chris King <chris.aa.king@gmail.com>
2019-09-18 19:44:13 +0200

    04 complete

--- Ruby-Projects/ruby-exercises/04_pig_latin/pig_latin.rb
+++ Ruby-Projects/ruby-exercises/04_pig_latin/pig_latin.rb
@@ -1,1 +1,23 @@
 #write your code here
+def translate x
+x = x.split(" ")
+
+# for each word, check to see if first letter starts with consonant
+# until first letter is vowel
+final = x.map { |word|
+    puts word + 'is the word'
+    letters = word.split ''
+    puts letters
+    while letters[0] != 'a' && letters[0] != 'e' && letters[0] != 'i' && letters[0] != 'o' do
+        puts letters[0]
+        letters.push(letters[0])
+        letters.shift()
+        puts letters[0] + 'after shift'
+    end
+    word = letters.join()
+    word = word.to_s + 'ay'
+}
+    final = final.join(" ")
+    final
+end
+translate("the quick brown fox")
